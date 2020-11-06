66d2d49cae61557c7e00526b7e7cec54f4cfe58c
khayam <khayam.hafezi@gmail.com>
2020-11-06 11:31:02 -0800

    create new class for blink button

--- main.py
+++ main.py
@@ -4,25 +4,49 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pyautogui
 import time, threading
-import tkinter
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class BlinkButton(QWidget):
+    def __init__(self, rect_w, freq):
+        super(BlinkButton, self).__init__()
+        self.timer = QTimer(self)
+        self.white = "white"
+        self.black = "black"
+        self.currentColor = self.white
+        self.styleSheet = "background-color: {}"
+        self.setGeometry(rect_w)
+        self.freq = freq
+
+    def start_show(self):
+        self.currentColor = self.white
+        self.setStyleSheet(self.styleSheet.format(self.currentColor))
+        self.show()
+        self.timer.timeout.connect(self.blink)
+        print(round((1.0/self.freq)*1000))
+        self.timer.start(round((1.0/self.freq)*1000))
+
+    @pyqtSlot()
+    def blink(self):
+        if self.currentColor == self.white:
+            self.currentColor = self.black
+            self.setStyleSheet(self.styleSheet.format(self.currentColor))
+        else:
+            self.currentColor = self.white
+            self.setStyleSheet(self.styleSheet.format(self.currentColor))
+
 
 def foo(window1, freq, redOn):
-    redStyle = "background-color: red"
-    blueStyle = "background-color: blue"
-    if redOn:
-        window1.setStyleSheet(redStyle)
-    else:
-        window1.setStyleSheet(blueStyle)
-    redOn = not redOn
-    # print(time.ctime())
+
     threading.Timer(freq, foo, args=(window1, freq, redOn)).start()
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 
 # Press the green button in the gutter to run the script.
@@ -55,27 +79,28 @@ if __name__ == '__main__':
     hOfWgtRight = 250
     wOfClickWgt = 130
     hOfClickWgt = 130
-    position = [[(w-wOfWgtTop)/2, 0], [0,(h-hOfWgtLeft)/2],
-                [(w-wOfWgtRight), (h-hOfWgtRight)/2], [0, h-hOfClickWgt],
-                [(w-wOfClickWgt), (h-hOfClickWgt)]]
+    position = [[(w - wOfWgtTop) / 2, 0], [0, (h - hOfWgtLeft) / 2],
+                [(w - wOfWgtRight), (h - hOfWgtRight) / 2], [0, h - hOfClickWgt],
+                [(w - wOfClickWgt), (h - hOfClickWgt)]]
     sizeOfButton = [[wOfWgtTop, hOfWgtTop], [wOfWgtLeft, hOfWgtLeft],
                     [wOfWgtRight, hOfWgtRight], [wOfClickWgt, hOfClickWgt], [wOfClickWgt, hOfClickWgt]]
     window = []
-    for i in range(0,5):
-
-        window.append(QWidget())
+    freqList = [12, 10, 7.5, 6.67, 8.57]
+    for i in range(0, 5):
+        pos = position[i]
+        btnSize = sizeOfButton[i]
+        rect = QRect(pos[0], pos[1], btnSize[0], btnSize[1])
+        window.append(BlinkButton(rect, freqList[i]))
         # layout = QVBoxLayout()
         # layout.addWidget(QPushButton('Top'))
         # layout.addWidget(QPushButton('Bottom'))
         # window.setLayout(layout)
-        window[i].setStyleSheet("background-color: red")
-        pos = position[i]
-        btnSize = sizeOfButton[i]
-        window[i].setGeometry(pos[0], pos[1], btnSize[0], btnSize[1])
-        window[i].setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        # window[i].setStyleSheet("background-color: red")
 
-        window[i].show()
-        foo(window[i], (i+1)/10, True)
+        # window[i].setGeometry(pos[0], pos[1], btnSize[0], btnSize[1])
+        window[i].setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        window[i].start_show()
+        # foo(window[i], (i + 1) / 10, True)
 
     app.exec_()
 
