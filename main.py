fef41a3bfdc73b88a29e0527fbc03be875f6acc3
khayam <khayam.hafezi@gmail.com>
2020-11-20 08:35:50 -0800

    add mouse movement by tcp port connection
    Signed-off-by: khayam <khayam.hafezi@gmail.com>

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+import socket
+import pyautogui
+import struct
+from threading import Thread
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 class BlinkButton(QWidget):
     def __init__(self, rect_w, freq):
@@ -39,9 +46,23 @@ class BlinkButton(QWidget):
             self.setStyleSheet(self.styleSheet.format(self.currentColor))
 
 
-def foo(window1, freq, redOn):
-
-    threading.Timer(freq, foo, args=(window1, freq, redOn)).start()
+def foo():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(5)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    continue
+                conn.sendall(b"Got IT")
+                print("Got it")
+                x = struct.unpack('!i', data[:4])[0]
+                y = struct.unpack('!i', data[4:8])[0]
+                #pyautogui.moveRel(x, y)  # move mouse 10 pixels down
+                pyautogui.moveTo(x, y)  # move mouse 10 pixels down
 
 
 def print_hi(name):
@@ -102,6 +123,11 @@ if __name__ == '__main__':
         window[i].start_show()
         # foo(window[i], (i + 1) / 10, True)
 
+    thread = Thread(target=foo)
+    thread.start()
     app.exec_()
+    thread.join()
+    print("thread finished...exiting")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
